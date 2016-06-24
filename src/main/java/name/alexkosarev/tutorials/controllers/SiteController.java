50138f8e58110ab1f3f64f71a5f65763fc049608
Alexander Kosarev <pvt.joke@gmail.com>
2016-06-24 11:01:25 +0500

    Поддержка JSP/JTSL в Standalone-режиме Упрощение конфига

--- src/main/java/name/alexkosarev/tutorials/controllers/SiteController.java
+++ src/main/java/name/alexkosarev/tutorials/controllers/SiteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class SiteController {
 
     @Autowired
